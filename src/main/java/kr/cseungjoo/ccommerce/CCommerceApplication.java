247405eef698fbce999664c7166e06a804b07c0e
CSeungJoo <seungjoo13579@gmail.com>
2024-09-24 19:49:55 +0900

    init

---
+++ src/main/java/kr/cseungjoo/ccommerce/CCommerceApplication.java
@@ -0,0 +1,13 @@
+package kr.cseungjoo.ccommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/cseungjoo/ccommerce/CCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.cseungjoo.ccommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
